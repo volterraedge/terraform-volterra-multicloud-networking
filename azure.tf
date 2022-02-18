dc182520b0787a762d436229face2d7064ad9c2c
alexcohen <alexcohen@ves.io>
2022-02-17 21:52:58 -0500

    try an empty features block with other vars

--- azure.tf
+++ azure.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  subscription_id             = var.azure_subscription_id
+  client_id                   = var.azure_client_id
+  client_secret               = var.azure_client_secret
+  tenant_id                   = var.azure_tenant_id
 }
 
 resource "azurerm_resource_group" "peer_vnet" {
